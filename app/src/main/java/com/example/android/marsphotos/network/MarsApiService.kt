e30f2c6ac31534c4b46c49b1da5c34aa7ae38832
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 13:03:47 +0530

    Updated gradle.

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,4 +1,7 @@
 package com.example.android.marsphotos.network
+//import retrofit2.Retrofit
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
+
+//private val retrofit = Retrofit.Builder()
