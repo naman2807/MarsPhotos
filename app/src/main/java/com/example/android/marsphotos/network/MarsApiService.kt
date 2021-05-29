3f26593e3d17f2487b8576f63efd384e9818a18f
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 13:07:39 +0530

    call build() to create the Retrofit object.

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -8,4 +8,4 @@ private const val BASE_URL =
 
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(BASE_URL)
+        .baseUrl(BASE_URL).build()
