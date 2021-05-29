e9df74761416048867e7e51edb18bcf1aa0d37dc
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 14:54:50 +0530

    Added comment in MarsPhoto.kt

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -2,6 +2,10 @@ package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Json
 
+/*
+* This data class defines a Mars photo which includes an ID, and the image URL.
+* The property names of this data class are used by Moshi to match the names of values in JSON.
+*/
 data class MarsPhoto(val id: String, @Json(name = "img_src") val imgSrcUrl: String)
 
 /**
