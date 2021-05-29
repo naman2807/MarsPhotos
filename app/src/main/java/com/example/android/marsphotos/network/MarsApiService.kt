2b8ff8d704b7e98218da1af8e47683781df25635
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 13:06:33 +0530

    call addConverterFactory() on the builder with an instance of ScalarsConverterFactory.

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,8 +1,10 @@
 package com.example.android.marsphotos.network
 
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
 
 private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
