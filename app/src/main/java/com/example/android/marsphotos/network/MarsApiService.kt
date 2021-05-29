952a6fb12c6a3ae3dbfcbfd3cdfe9d6b501d87cb
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 13:10:07 +0530

    Use the @GET annotation to tell Retrofit that this is GET request, and specify endpoint, for that web service method.

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.marsphotos.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
@@ -11,5 +12,6 @@ private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL).build()
 
 interface MarsApiService{
-        fun getPhotos()
+    @GET("/photos")
+    fun getPhotos() : String
 }
