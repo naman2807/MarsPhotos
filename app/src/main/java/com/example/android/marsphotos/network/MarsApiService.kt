a43f2187e8ac51bde676da508250234b7b8483a6
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 14:37:17 +0530

    For Moshi's annotations to work properly with Kotlin, in the Moshi builder, add the KotlinJsonAdapterFactory, and then call build().

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,6 +1,7 @@
 package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
@@ -14,6 +15,8 @@ private val retrofit = Retrofit.Builder()
         .build()
 
 private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 interface MarsApiService{
 
