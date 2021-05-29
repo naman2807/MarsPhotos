d95db1582c4b9d7a9ecba45dbd7790bf44488374
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 14:53:28 +0530

    To print the number of photos retrieved update _status.value

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -48,7 +48,7 @@ class OverviewViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val listResult = MarsApi.retrofitService.getPhotos()
-                _status.value = listResult
+                _status.value = "Success: ${listResult.size} Mars photos retrieved"
             }catch (e : Exception){
                 _status.value = "Status Failure: $e"
             }
