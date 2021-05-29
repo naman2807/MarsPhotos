447fc38bca33432393af5ab9e1c253b0845d8bc1
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-29 14:52:05 +0530

    Update the MarsApiService interface to have Retrofit return a list of MarsPhoto objects, instead of returning String

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -21,7 +21,7 @@ private val retrofit = Retrofit.Builder()
 interface MarsApiService{
 
     @GET("photos")
-    suspend fun getPhotos() : String
+    suspend fun getPhotos() : List<MarsPhoto>
 }
 
 object MarsApi{
