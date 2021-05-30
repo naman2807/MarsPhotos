71d228faa671fff0976b9a3f8c16f4bd8f58abf8
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-30 10:05:30 +0530

    Annotate the function with @BindingAdapter

--- app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
@@ -1,8 +1,10 @@
 package com.example.android.marsphotos.overview
 
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 
 class BindingAdapters {
+    @BindingAdapter("imageURL")
     fun bindImage(imgView : ImageView, imgSrc : String){
 
     }
