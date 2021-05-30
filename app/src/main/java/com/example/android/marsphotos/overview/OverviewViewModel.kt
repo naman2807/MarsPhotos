cdcd4972bc54727492c1f02408e77638ca053f88
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-30 09:48:50 +0530

    In overview/OverviewViewModel.kt. Just below the _status property declaration, add a new mutable property called _photos, of the type MutableLiveData

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
 
 /**
@@ -30,6 +31,8 @@ class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<String>()
+
+    private val _photo = MutableLiveData<MarsPhoto>()
 
     // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
