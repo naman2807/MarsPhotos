c8c98365612304e6015589dcfe01127426a77755
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-31 11:05:27 +0530

    Added details of ListAdapter in OverViewListAdapter.kt

--- app/src/main/java/com/example/android/marsphotos/overview/OverViewListAdapter.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverViewListAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.android.marsphotos.overview
 
 class OverViewListAdapter {
+
+
 }
+/*
+ListAdapter is a subclass of the RecyclerView.Adapter class for presenting List data in a
+RecyclerView, including computing diffs between Lists on a background thread.
+
+In this app you will use the DiffUtil implementation in the ListAdapter. The advantage of using
+ DiffUtil is every time some item in the RecyclerView is added, removed or changed, the whole list
+ doesn't get refreshed. Only the items that have been changed are refreshed.
+ */
