962352c4cdafc75913a347a643eda85e2c2dc33a
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-31 11:38:28 +0530

    At the end of bindRecyclerView() function, call adapter.submitList() with the Mars photos list data. This tells the RecyclerView when a new list is available.

--- app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
@@ -23,6 +23,7 @@ import com.example.android.marsphotos.network.MarsPhoto
 @BindingAdapter("listData")
     fun bindRecyclerView(recyclerView: RecyclerView, data : List<MarsPhoto>){
         val adapter = recyclerView.adapter as OverViewListAdapter
+        adapter.submitList(data)
     }
 
 /*
